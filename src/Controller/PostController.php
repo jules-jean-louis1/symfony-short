5d1157bc36cd24f7069f8a23b21c775e22ca84c1
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-06-25 09:35:46 +0200

    Feat: add edit show post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Entity\User;
 use App\Form\Type\PostAddType;
 use App\Repository\PostRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,14 +23,6 @@ class PostController extends AbstractController
     {
         $this->entityManager = $entityManager;
         $this->postRepository = $postRepository;
-    }
-
-    #[Route('/posts', name: 'app_post')]
-    public function index(): Response
-    {
-        return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
     }
 
     #[Route('/post/add', name: 'app_post_add')]
@@ -48,25 +42,52 @@ class PostController extends AbstractController
             $user = $this->entityManager->getRepository(User::class)->find($userData->getId());
 
             $post->setOwner($user);
-            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setCreatedAt(new DateTimeImmutable());
 
             $this->entityManager->persist($post);
             $this->entityManager->flush();
 
             $this->addFlash('success', 'Post ajouté avec succès.');
-            return $this->redirectToRoute('app_post');
+            return $this->redirectToRoute('app_posts');
         }
-        return $this->render('post/add/index.html.twig', [
+        return $this->render('post/add.html.twig', [
             'formAddPost' => $form->createView(),
         ]);
     }
 
+    #[Route('/post/{id}/edit', name: 'app_post_edit')]
+    public function edit(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $form = $this->createForm(PostAddType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_posts');
+        }
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'formEdit' => $form
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'app_show_post')]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
     #[Route('/posts', name: 'app_posts')]
-    public function getAllPost(): Response
+    public function getAll(): Response
     {
         $posts = $this->postRepository->findAll();
         return $this->render('posts/index.html.twig', [
             'posts' => $posts
         ]);
     }
+
 }
